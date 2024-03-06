629a32cc42408080d0269a3eeda8ddd29e8eb727
Michaelj520 <156280702+Michaelj520@users.noreply.github.com>
2024-03-06 12:55:15 -0800

    .

--- grade.sh
+++ grade.sh
@@ -14,3 +14,46 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cp student-submission/*.java grading-area
+cp TestListExamples.java grading-area 
+cp -r lib grading-area
+
+cd grading-area
+
+
+if ! [ -f ListExamples.java ]
+then 
+    echo "Missing ListExamples.java in student submission"
+    echo "Score: 0"
+    exit
+fi
+
+javac -cp $CPATH *.java &> compile.txt 
+if [ $? -ne 0 ]
+then
+    echo "Compilation Error"
+    echo "Score: 0"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > outputResults 2>&1
+
+SUCCESS=$(grep "OK" outputResults)
+
+if [[ -n $SUCCESS ]]
+then 
+    echo "Everything passed! Your score is 100%"
+else
+    totalTests=$(grep -oE "Tests run: [0-9]+" outputResults | grep -oE "[0-9]+")
+    Failures=$(grep -oE "Failures: [0-9]+" outputResults | grep -oE "[0-9]+")
+    Success=$((totalTests - Failures))
+    Percentage=$(echo "scale=2; $Success * 100 / $totalTests" | bc)
+    echo "Score: $Percentage%"
+fi
+
+
+
+
+
+
